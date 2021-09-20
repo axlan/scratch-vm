ea53089f2b5bec0beee8ce024af96f49c7a3c729
jdiamond <feros32@gmail.com>
2021-09-20 05:43:48 +0000

    Basic LED on demo

--- src/extensions/scratch3_newblocks/index.js
+++ src/extensions/scratch3_newblocks/index.js
@@ -3,6 +3,42 @@ const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
 const log = require('../../util/log');
 
+//import ky from 'ky';
+
+function sendData(args) {
+    const http = require('http')
+
+    const data = new TextEncoder().encode(JSON.stringify(args))
+
+    const options = {
+        hostname: '192.168.1.123',
+        port: 80,
+        path: '/json/state',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type'
+        }
+    }
+
+    const req = http.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+    })
+
+    req.on('error', error => {
+        console.error(error)
+    })
+
+    req.write(data)
+    req.end()
+    // const json = await ky.post('http://192.168.1.123/json/state', {json: args}).json();
+
+    // console.log(json);
+}
+
 class Scratch3NewBlocks {
     constructor (runtime) {
         this.runtime = runtime;
@@ -23,6 +59,16 @@ class Scratch3NewBlocks {
                             defaultValue: "hello"
                         }
                     }
+                },
+                {
+                    opcode: 'ledsOff',
+                    blockType: BlockType.COMMAND,
+                    text: 'turn LEDs off'
+                },
+                {
+                    opcode: 'ledsOn',
+                    blockType: BlockType.COMMAND,
+                    text: 'turn LEDs on'
                 }
             ],
             menus: {
@@ -33,6 +79,21 @@ class Scratch3NewBlocks {
     writeLog (args) {
         const text = Cast.toString(args.TEXT);
         log.log(text);
+    }
+
+
+    
+
+    ledsOff (args) {
+        sendData({
+            on:false
+        })
+    }
+
+    ledsOn (args) {
+        sendData({
+            on:true
+        })
     }
 }
 
