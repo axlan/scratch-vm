d22841554425309ff48d4114ec36f6b5304c90cd
jdiamond <feros32@gmail.com>
2021-09-21 06:58:38 +0000

    start per segment control

--- src/extensions/scratch3_newblocks/index.js
+++ src/extensions/scratch3_newblocks/index.js
@@ -6,16 +6,22 @@ const formatMessage = require('format-message');
 const http = require('http')
 const log = require('../../util/log');
 
-/**
- * Enum for pen color parameter values.
- * @readonly
- * @enum {string}
- */
- const ColorParam = {
-    COLOR: 'color',
-    SATURATION: 'saturation',
-    BRIGHTNESS: 'brightness',
-    TRANSPARENCY: 'transparency'
+const SEGMENTS = {
+    eagle: {
+        start: 0,
+        len: 16,
+        id: 0
+    },
+    deer: {
+        start: 16,
+        len: 16,
+        id: 1
+    },
+    lion: {
+        start: 32,
+        len: 16,
+        id: 2
+    }
 };
 
 function sendData(args) {
@@ -28,10 +34,7 @@ function sendData(args) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Content-Length': data.length,
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-            'Access-Control-Allow-Headers': 'Content-Type'
+            'Content-Length': data.length
         }
     }
 
@@ -50,9 +53,85 @@ function sendData(args) {
     // console.log(json);
 }
 
+function initDisplay() {
+    seg_data = []
+    for(var key in SEGMENTS) {
+        seg_data.push({
+            start: SEGMENTS[key].start,
+            len: 16,
+            on: false,
+            fx: 0
+        });
+    }
+    sendData({
+        seg: seg_data
+    });
+}
+
 class Scratch3NewBlocks {
     constructor (runtime) {
         this.runtime = runtime;
+
+        this._onTargetCreated = this._onTargetCreated.bind(this);
+
+        runtime.on('targetWasCreated', this._onTargetCreated);
+
+        initDisplay();
+    }
+
+    /**
+     * The key to load & store a target's pen-related state.
+     * @type {string}
+     */
+    static get STATE_KEY () {
+        return 'Scratch.leds';
+    }
+
+    /**
+     * @param {Target} target - collect pen state for this target. Probably, but not necessarily, a RenderedTarget.
+     * @returns {PenState} the mutable pen state associated with that target. This will be created if necessary.
+     * @private
+     */
+    _getState (target) {
+        let state = target.getCustomState(Scratch3NewBlocks.STATE_KEY);
+        if (!state) {
+            state = {segment: Object.keys(SEGMENTS)[0]};
+            target.setCustomState(Scratch3NewBlocks.STATE_KEY, state);
+        }
+        return state;
+    }
+    
+
+    /**
+     * Initialize color parameters menu with localized strings
+     * @returns {array} of the localized text and values for each menu element
+     * @private
+     */
+     _initSegmentName() {
+        items = [];
+        for(var key in SEGMENTS) {
+            items.push({
+                text: key,
+                value: key
+            });
+        }
+        return items;
+    }
+
+    /**
+     * When a pen-using Target is cloned, clone the pen state.
+     * @param {Target} newTarget - the newly created target.
+     * @param {Target} [sourceTarget] - the target used as a source for the new clone, if any.
+     * @listens Runtime#event:targetWasCreated
+     * @private
+     */
+    _onTargetCreated (newTarget, sourceTarget) {
+        if (sourceTarget) {
+            const state = sourceTarget.getCustomState(Scratch3NewBlocks.STATE_KEY);
+            if (state) {
+                newTarget.setCustomState(Scratch3NewBlocks.STATE_KEY, Clone.simple(state));
+            }
+        }
     }
 
 
@@ -96,8 +175,28 @@ class Scratch3NewBlocks {
                         description: 'set the LED color to a particular (RGB) value'
                     }),
                 },
+                {
+                    opcode: 'selectLedSegment',
+                    blockType: BlockType.COMMAND,
+                    arguments: {
+                        SEGMENT_NAME: {
+                            type: ArgumentType.STRING,
+                            menu: 'segmentName',
+                            defaultValue: Object.keys(SEGMENTS)[0]
+                        },
+                    },
+                    text: formatMessage({
+                        id: 'newblocks.selectLedSegment',
+                        default: 'control [SEGMENT_NAME] segment',
+                        description: 'select the LED segment to control'
+                    }),
+                },
             ],
             menus: {
+                segmentName: {
+                    acceptReporters: true,
+                    items: this._initSegmentName()
+                }
             }
         };
     }
@@ -116,11 +215,15 @@ class Scratch3NewBlocks {
      */
      setLedColor (args, util) {
         const rgb = Cast.toRgbColorObject(args.COLOR);
+        const target = util.target;
+        var state = this._getState(target)
         sendData({
-            "seg": [{	
+            "seg": [{
+              "id": SEGMENTS[state.segment].id,
               "col": [	
                 [rgb.r, rgb.g, rgb.b]	
-              ]		
+              ],
+              "on": true
             }]	
         })
     }
@@ -136,6 +239,13 @@ class Scratch3NewBlocks {
         sendData({
             on:true
         })
+    }
+
+    selectLedSegment (args, util) {
+        const target = util.target;
+        var state = this._getState(target);
+        state.segment = args.SEGMENT_NAME;
+        target.setCustomState(Scratch3NewBlocks.STATE_KEY, state);
     }
 }
 
